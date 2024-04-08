1abea4cff8ae51f631962d0526de300231b97095
CarloLaguda <carlo.laguda07@gmail.com>
2024-04-08 15:54:54 +0200

    V1.0.3 HTML, CSS, JS MOD, 18FLG

--- Javascript 4/script.js
+++ Javascript 4/script.js
@@ -1,7 +1,40 @@
-let videogiochi
+let videogiochi;
+let isAdult = false;
+const note = document.getElementById("p_Note")
+const S_videogame = document.getElementById("Select_Videogame")
 
-window.onload(
-    fetch('magazzino.json').then(response => response.json()).then(data => {
-        videogiochi = data.videogiochi
-    }).catch(error => console.log("Errore nel caricamento: ", error))
-)
+// La funzione window.onload deve essere assegnata con una funzione di callback
+window.onload = function() {
+    fetch('magazzino.json')
+        .then(response => response.json())
+        .then(data => {
+            videogiochi = data.videogiochi;
+            // Qui puoi fare qualcosa con i dati se necessario
+        })
+        .catch(error => console.log("Errore nel caricamento: ", error));
+
+    checkAge();
+};
+
+
+// Funzione per impostare l'età
+function setAge(isAdultValue) {
+    isAdult = isAdultValue;
+    document.getElementById("agePopup").style.display = "none";
+    note.textContent = isAdult ? "true" : "false";
+    if (isAdult) {
+        document.body.classList.remove("agePopupOpen");
+        note.textContent = isAdult;
+        S_videogame.disabled = false;
+
+    } else {
+        document.body.classList.add("agePopupOpen");
+        note.textContent = isAdult;
+    }
+}
+
+function checkAge() {
+    document.getElementById("agePopup").style.display = "block";
+}
+
+checkAge();
