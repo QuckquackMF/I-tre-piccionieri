fdb0828f0977bd2e2b170149cb3537c58fe25ece
net <net@SIS-21.igalvani.local>
2024-04-12 08:57:39 +0200

    Beta (V1.1.1 json working)

--- Javascript 4/script.js
+++ Javascript 4/script.js
@@ -1,39 +1,22 @@
-//#region variabili/costanti iniziali e JSON
-
-let json = {
-    "videogiochi": [
-        {"Name":"Gran Turismo 7", "InStock":"30", "Compatibility":["Playstation 4", "Playstation 5"], "Pegi":"3", "Price":"79.99"},
-        {"Name":"Uncharted: The Nathan Drake Collection", "InStock":"27", "Compatibility":["Playstation 4"], "Pegi":"16", "Price":"19.99"},
-        {"Name":"Uncharted: Legacy of Thieves Collection", "InStock":"30", "Compatibility":["Playstation 5", "Windows"], "Pegi":"16", "Price":"49.99"},
-        {"Name":"Grand Theft Auto 5", "InStock":"56", "Compatibility":["Playstation 3", "Playstation 4", "Playstation 5", "Xbox 360", "Xbox One", "Xbox Series X/S", "Windows"], "Pegi":"18", "Price":"25"},
-        {"Name":"Hearts of Iron 4", "InStock":"45", "Compatibility":["Linux", "Windows", "MacOS"], "Pegi":"7", "Price":"7.99"},
-        {"Name":"God of War", "InStock":"10", "Compatibility":["Playstation 4", "Windows"], "Pegi":"18", "Price":"20"},
-        {"Name":"Minecraft", "InStock":"15", "Compatibility":["Playstation 3", "Playstation 4", "Playstation 5", "Xbox 360", "Xbox One", "Xbox Series X/S", "Windows", "Linux", "IOS", "Android", "Nintendo Switch", "Nintendo 3DS", "MacOS"], "Pegi":"7", "Price":"18.99"},
-        {"Name":"Forza Horizon 7", "InStock":"30", "Compatibility":["Xbox One", "Windows"], "Pegi":"3", "Price":"49.99"},
-        {"Name":"Call of Duty: Modern Warfare 3", "InStock":"80", "Compatibility":["Playstation 5", "Playstation 4", "Xbox One", "Xbox Series X/S", "Windows"], "Pegi":"18", "Price":"57.99"},
-        {"Name":"FC 24", "InStock":"60", "Compatibility":["Playstation 5", "Playstation 4", "Xbox One", "Xbox Series X/S", "Windows", "Android", "Nintendo Switch"], "Pegi":"3", "Price":"79.99"},
-        {"Name":"F1 23", "InStock":"39", "Compatibility":["Playstation 5", "Playstation 4", "Xbox One", "Xbox Series X/S", "Windows"], "Pegi":"3", "Price":"79.99"},
-        {"Name":"Helldivers II", "InStock":"0", "Compatibility":["Playstation 5", "Windows"], "Pegi":"18", "Price":"39.99"},
-        {"Name":"Baldur's Gate 3", "InStock":"110", "Compatibility":["Playstation 5", "Xbox Series X/S", "Windows", "MacOS"], "Pegi":"18", "Price":"69.99"}
-    ]
-};
 
 window.onload = function() {
-    granTurismo7 = new Videogame(json.videogiochi[0].Name, json.videogiochi[0].InStock, json.videogiochi[0].Compatibility, json.videogiochi[0].Pegi, json.videogiochi[0].Price);
-    unchartedTNDC = new Videogame(json.videogiochi[1].Name, json.videogiochi[1].InStock, json.videogiochi[1].Compatibility, json.videogiochi[1].Pegi, json.videogiochi[1].Price);
-    unchartedLOTC = new Videogame(json.videogiochi[2].Name, json.videogiochi[2].InStock, json.videogiochi[2].Compatibility, json.videogiochi[2].Pegi, json.videogiochi[2].Price);
-    grandTheftAuto5 = new Videogame(json.videogiochi[3].Name, json.videogiochi[3].InStock, json.videogiochi[3].Compatibility, json.videogiochi[3].Pegi, json.videogiochi[3].Price);
-    heartsOfIron4 = new Videogame(json.videogiochi[4].Name, json.videogiochi[4].InStock, json.videogiochi[4].Compatibility, json.videogiochi[4].Pegi, json.videogiochi[4].Price);
-    godOfWar = new Videogame(json.videogiochi[5].Name, json.videogiochi[5].InStock, json.videogiochi[5].Compatibility, json.videogiochi[5].Pegi, json.videogiochi[5].Price);
-    minecraft = new Videogame(json.videogiochi[6].Name, json.videogiochi[6].InStock, json.videogiochi[6].Compatibility, json.videogiochi[6].Pegi, json.videogiochi[6].Price);
-    forzaHorizon7 = new Videogame(json.videogiochi[7].Name, json.videogiochi[7].InStock, json.videogiochi[7].Compatibility, json.videogiochi[7].Pegi, json.videogiochi[7].Price);
-    callOfDutyMW3 = new Videogame(json.videogiochi[8].Name, json.videogiochi[8].InStock, json.videogiochi[8].Compatibility, json.videogiochi[8].Pegi, json.videogiochi[8].Price);
-    fc24 = new Videogame(json.videogiochi[9].Name, json.videogiochi[9].InStock, json.videogiochi[9].Compatibility, json.videogiochi[9].Pegi, json.videogiochi[9].Price);
-    f123 = new Videogame(json.videogiochi[10].Name, json.videogiochi[10].InStock, json.videogiochi[10].Compatibility, json.videogiochi[10].Pegi, json.videogiochi[10].Price);
-    helldiversII = new Videogame(json.videogiochi[11].Name, json.videogiochi[11].InStock, json.videogiochi[11].Compatibility, json.videogiochi[11].Pegi, json.videogiochi[11].Price);
-    baldursGate3 = new Videogame(json.videogiochi[12].Name, json.videogiochi[12].InStock, json.videogiochi[12].Compatibility, json.videogiochi[12].Pegi, json.videogiochi[12].Price);
+    fetch('magazzino.json').then(response => response.json()).then(json => {
+        granTurismo7 = new Videogame(json.videogiochi[0].Name, json.videogiochi[0].InStock, json.videogiochi[0].Compatibility, json.videogiochi[0].Pegi, json.videogiochi[0].Price);
+        unchartedTNDC = new Videogame(json.videogiochi[1].Name, json.videogiochi[1].InStock, json.videogiochi[1].Compatibility, json.videogiochi[1].Pegi, json.videogiochi[1].Price);
+        unchartedLOTC = new Videogame(json.videogiochi[2].Name, json.videogiochi[2].InStock, json.videogiochi[2].Compatibility, json.videogiochi[2].Pegi, json.videogiochi[2].Price);
+        grandTheftAuto5 = new Videogame(json.videogiochi[3].Name, json.videogiochi[3].InStock, json.videogiochi[3].Compatibility, json.videogiochi[3].Pegi, json.videogiochi[3].Price);
+        heartsOfIron4 = new Videogame(json.videogiochi[4].Name, json.videogiochi[4].InStock, json.videogiochi[4].Compatibility, json.videogiochi[4].Pegi, json.videogiochi[4].Price);
+        godOfWar = new Videogame(json.videogiochi[5].Name, json.videogiochi[5].InStock, json.videogiochi[5].Compatibility, json.videogiochi[5].Pegi, json.videogiochi[5].Price);
+        minecraft = new Videogame(json.videogiochi[6].Name, json.videogiochi[6].InStock, json.videogiochi[6].Compatibility, json.videogiochi[6].Pegi, json.videogiochi[6].Price);
+        forzaHorizon7 = new Videogame(json.videogiochi[7].Name, json.videogiochi[7].InStock, json.videogiochi[7].Compatibility, json.videogiochi[7].Pegi, json.videogiochi[7].Price);
+        callOfDutyMW3 = new Videogame(json.videogiochi[8].Name, json.videogiochi[8].InStock, json.videogiochi[8].Compatibility, json.videogiochi[8].Pegi, json.videogiochi[8].Price);
+        fc24 = new Videogame(json.videogiochi[9].Name, json.videogiochi[9].InStock, json.videogiochi[9].Compatibility, json.videogiochi[9].Pegi, json.videogiochi[9].Price);
+        f123 = new Videogame(json.videogiochi[10].Name, json.videogiochi[10].InStock, json.videogiochi[10].Compatibility, json.videogiochi[10].Pegi, json.videogiochi[10].Price);
+        helldiversII = new Videogame(json.videogiochi[11].Name, json.videogiochi[11].InStock, json.videogiochi[11].Compatibility, json.videogiochi[11].Pegi, json.videogiochi[11].Price);
+        baldursGate3 = new Videogame(json.videogiochi[12].Name, json.videogiochi[12].InStock, json.videogiochi[12].Compatibility, json.videogiochi[12].Pegi, json.videogiochi[12].Price);
+    }).catch(error => console.log("Errore nel caricamento: ", error))
+    
 };
-
 //costanti contenenti elementi HTML
 const note = document.getElementById("p_Note");
 const S_videogame = document.getElementById("Select_Videogame");
@@ -196,16 +179,16 @@ buy.addEventListener('click', function(){
             getObject().inStock -= parseInt(inputNumber.value);
             copieRimaste.textContent = "Copie rimaste: " + getObject().inStock;
             inputNumber.value = 0;
-
-            console.log("Acquisto riuscito! " + value);
+            price.textContent = "";
+            alert("Acquisto  andato a abuon fine")
         },
 
         function(error){
-            console.log("Aquisto fallito! " + error);
+
+            alert("Acquisto Non andato a abuon fine")
         }
     )
 })
-
 /*la funzione getObject viene chiamata quando serve 
 prendere l'oggetto corrispondente al videogioco 
 selezionato per poterne accedere gli attributi*/ 
@@ -239,7 +222,6 @@ function getObject(){
             return baldursGate3;
     }
 }
-
 document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll(".game-images img");
     const indicators = document.querySelectorAll('.image-indicators .indicator');
